1814eb02e83a1a4f588f5e957bd73a4c6b0dfa27
OnePaperHoon <wkdgns720@naver.com>
2025-12-18 22:16:42 +0900

    add

--- 00.Algorithms/00.Bubble Sort/main.c
+++ 00.Algorithms/00.Bubble Sort/main.c
@@ -13,11 +13,14 @@ int bubble_sort(int arr[], size_t arr_size)
     // 입력 유효성 검사
     if (arr == NULL || arr_size == 0)
         return -1;
+    size_t arr_len = arr_size;
+    int is_sorted = 0;
 
     // 버블 정렬 알고리즘 구현
     for (size_t i = 0; i < arr_size - 1; i++)
     {
-        for (size_t j = 0; j < arr_size - i - 1; j++)
+        is_sorted = 1; // 정렬 여부 플래그 초기화
+        for (size_t j = 0; j < arr_len; j++)
         {
             // 인접한 요소 비교 및 교환
             if (arr[j] > arr[j + 1])
@@ -26,8 +29,11 @@ int bubble_sort(int arr[], size_t arr_size)
                 int temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
+                is_sorted = 0; // 교환이 발생했으므로 정렬되지 않음
             }
         }
+        arr_len--; // 마지막 요소는 이미 정렬되었으므로 비교 범위 축소
+        // 정렬이 완료되었으면 종료
     }
 
     return 0;
